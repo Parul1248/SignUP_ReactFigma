10511686c648d9a46475d021ba5f045c0b09ba68
Kunwar Singh <Kunwar_Singh@steris.com>
2022-10-31 23:26:29 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="signup-box">
+          <div className="left-img">
+            <img height={800} width={700} src={window.location.origin + '/signup2.png'} />
+          </div>
+          <div className='align-item'>
+            <h2 className='h2'>
+              Business news delivered
+              weekly.
+            </h2>
+            <span>
+              <p className='p-text'>
+                Stay up to date with the latest articles and business
+                updates. You'll even get special recommendations
+                weekly.
+              </p>
+            </span>
+            <div className='signup-input-box'>
+              <input className='input' type={"text"} placeholder="Your Email" />
+              <div className='signup-button'>
+                Subscribe
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
